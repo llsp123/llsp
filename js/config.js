e9dcc3e3e1f36a7fc870da70531ca681e2fde649
backend-1 <backend-1@example.com>
2026-05-10 10:43:27 +0800

    引入官网变量

---
+++ js/config.js
@@ -0,0 +1,23 @@
+// 全局配置
+(function() {
+    window.APP_CONFIG = {
+        img: "img/0.jpg",
+        title: "懒懒视频最全的影视APP - lanlansp.com",
+        app_name: "懒懒视频",
+        www: "www.lanlansp.com",
+        fzdz: "www.lanlansp.com",
+        gwdz: "http://www.lanlansp.com2/",
+        qqqun: '<p class="p2">土豆交流群</p>',
+        qundz: "https://ptdy.org/joinchat/RDlHk2k3K0EMerNV01wu4Q", // 加群地址
+        app_gg: "2.3.6版本优化崩溃问题 修复播放稳定性",
+        zxzt: "2.4.19版本！支持安卓4.5以上", // 安卓下载提示
+        zxzd: "https://wwic.lanzouo.com/ifT4f2y45m9c", // 安卓下载地址
+        cxzt: "IOS1.0.1测试阶段仅支持播放，请安装后体验", // 苹果下载提示
+        cxzd: "https://mfsp551.lanzouv.com/iL7dv3oaon6b", // 苹果下载地址
+        kfnd: "2022",
+        kk: "1",
+        qrCodeBase64: ""   // 动态二维码生成时填充
+    };
+    // 网页标题
+    document.title = window.APP_CONFIG.title;
+})();
